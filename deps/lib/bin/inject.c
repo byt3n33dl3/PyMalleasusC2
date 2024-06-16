a4bf5b25095e1494b3e14fb5265951e603a1574f
Sulaiman <f22dm0486@student.nafa.edu.sg>
2024-06-16 12:46:00 +0700

    pegasus include
    C doc

--- deps/lib/bin/inject.c
+++ deps/lib/bin/inject.c
@@ -1,27 +1,10 @@
-/*
-Cuckoo Sandbox - Automated Malware Analysis.
-Copyright (C) 2010-2015 Cuckoo Foundation.
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
 #include <stdio.h>
 #include <wchar.h>
 #include <inttypes.h>
 #include <windows.h>
 #include <tlhelp32.h>
 #include <shlwapi.h>
+#include <pegasus.h>
 #include "../inc/ntapi.h"
 
 #define INJECT_NONE 0
